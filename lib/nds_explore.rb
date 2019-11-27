66300b8adea36d7905189b7071ec45e8dc06a702
kowalix007 <kowalix007@gmail.com>
2019-11-27 23:06:24 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,6 +11,11 @@ end
 
 def print_first_directors_movie_titles
 
-pp directors_database
+  row_index = 0
 
+ while row_index < directors_database[0][:movies].length do
+
+     puts directors_database[0][:movies][row_index][:title]
+     row_index += 1
+    end
 end
